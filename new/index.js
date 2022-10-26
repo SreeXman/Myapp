880550c0bc087e2a96b275597191f1e26053aed3
edward sreeman <104483305+SreeXman@users.noreply.github.com>
2022-10-26 14:04:50 +0530

    Add files via upload

---
+++ new/index.js
@@ -0,0 +1,16 @@
+// -----Country Code Selection
+// $("#mobile_code").intlTelInput({
+//     initialCountry: "in",
+//     separateDialCode: true,
+//     // utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/11.0.4/js/utils.js"
+// });
+
+function show_alert() {
+    alert("Thank you");
+}
+
+function showHide(checked) {
+    if (checked == "true")
+        $("#class-ten").fadeIn();
+    else $("#class-ten").fadeOut();
+}
